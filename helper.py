675d932ee4593da3ad7c286a2d94e37cc7f6d0b8
CodeUpdaterFXD <140302323+CodeUpdaterFXD@users.noreply.github.com>
2025-08-29 19:49:55 -0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -93,8 +93,8 @@ def main():
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
         }
 
-        #payload = json.dumps({'content': f'```txt{message}```'})
-        payload = json.dumps({'System: {system}\n content': f'```txt{message}```'})
+        payload = json.dumps({'content': f'```txt{message}```'})
+        #payload = json.dumps({'System: {system}\n content': f'```txt{message}```'})
         #payload = json.dumps({'content': f'```txt\nSystem: {system}\nMessage: {message}```'})
 
         try:
